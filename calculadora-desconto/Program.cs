c36ee5a52c0349503274f49008b8b651f46d8b20
Rawdrey Nobre <rawdrey@gmail.com>
2024-03-12 08:39:10 -0300

    iniciando

---
+++ calculadora-desconto/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadora_desconto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Incira o nome do produto:");
+            string produto = Console.ReadLine();
+            Console.WriteLine("Incira o preço:");
+            double preco = double.Parse(Console.ReadLine());
+            Console.WriteLine("Incira a quantidade");
+            int quantidade = Console.ReadLine();
+
+
+        }
+    }
+}
